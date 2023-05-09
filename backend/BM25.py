84d87dab2098fe1f0f45a6d856d8af7fd39a7064
Tingyin Ding <32661494+Tinameow@users.noreply.github.com>
2023-05-08 22:00:41 -0500

    add synonym

--- backend/BM25.py
+++ backend/BM25.py
@@ -5,7 +5,10 @@ from build_model import readQuestions
 from build_model import readAnswers
 from build_model import readTags
 from gensim.utils import tokenize as tk
-
+from process_word import process_list_of_word
+from synonym import is_synonym
+import pickle
+import os
 
 def tokenize(original_list):
     # print(original_list)
@@ -40,6 +43,7 @@ class bm25Model:
 
         df = {}
         for document in documents:
+            document = process_list_of_word(document)
             temp = {}
             for word in document:
                 if word in temp.keys():
@@ -60,12 +64,19 @@ class bm25Model:
         score = 0.0
         num_of_word_in_doc = len(self.f[index])
         # print("query:", query)
+        query = process_list_of_word(query)
         qf = Counter(query)
         for word in query:
-            if word not in self.f[index]:
-                continue
-            numerator1 = self.idf[word] * (self.f[index][word] * (self.k1+1))
-            denominator1 = self.f[index][word] + self.k1 * (1 - self.b + self.b * num_of_word_in_doc / self.avg_doc_len)
+            doc_word = word
+            if doc_word not in self.f[index]:
+                for w in self.f[index]:
+                    if is_synonym(w, doc_word):
+                        doc_word = w
+                        break
+                else:
+                    continue
+            numerator1 = self.idf[doc_word] * (self.f[index][doc_word] * (self.k1+1))
+            denominator1 = self.f[index][doc_word] + self.k1 * (1 - self.b + self.b * num_of_word_in_doc / self.avg_doc_len)
             numerator2 = qf[word] * (self.k2+1)
             denominator2 = qf[word] + self.k2
             score += (numerator1/denominator1) * (numerator2/denominator2)
@@ -78,13 +89,11 @@ class bm25Model:
 
         return score_list
 
-def query(query):
-    # TODO
-    query = query.strip().split()
-
-    filepath_Q = 'data/Questions.csv'
-    filepath_A = 'data/Answers.csv'
-    filepath_T = 'data/Tags.csv'
+def train():
+    dir_name = os.path.dirname(__file__)
+    filepath_Q = dir_name + '/data/Questions.csv'
+    filepath_A = dir_name + '/data/Answers.csv'
+    filepath_T = dir_name + '/data/Tags.csv'
 
     my_dict = {}
 
@@ -102,62 +111,80 @@ def query(query):
     title_list_after_tok = tokenize(title_list)
 
     my_model = bm25Model()
-    my_model.input_document(title_list_after_tok[0:10])
-    # print(my_model.idf)
-    score_list = my_model.get_score_list(query)
+    my_model.key_list = key_list
+    my_model.my_dict = my_dict
+    my_model.input_document(title_list_after_tok[0:100])
+    with open(dir_name + '/bm25.pickle', 'wb') as f:
+        pickle.dump(my_model, f)
 
+
+def query(query):
+    # TODO
+    query = query.strip().split()
+    my_model = None
+    dir_name = os.path.dirname(__file__)
+    with open(dir_name + '/bm25.pickle', 'rb') as f:
+        my_model = pickle.load(f)
+    # if not my_model:
+    #     raise Exception()
+    
+    score_list = my_model.get_score_list(query)
     # print(score_list[0:10])
 
-    combined_list = [(k, s) for k, s in zip(key_list, score_list) if s > 0]
+    combined_list = [(k, s) for k, s in zip(my_model.key_list, score_list) if s > 0]
     # combined_list = list(zip(key_list, score_list))
     sorted_combined_list = sorted(combined_list, reverse=True, key=lambda x: x[1])
     sorted_key_list = [x[0] for x in sorted_combined_list]
     # print(sorted_key_list[0:10])
 
-    return sorted_key_list, my_dict
+    return sorted_key_list[:min(len(sorted_key_list), 10)], my_model.my_dict
 
 if __name__ == '__main__':
+    train()
+    key_list, my_dict = query("well sql")
+    for k in key_list:
+        print(my_dict[k].title)
 
-    filepath_Q = 'data/Questions.csv'
-    filepath_A = 'data/Answers.csv'
-    filepath_T = 'data/Tags.csv'
+    # filepath_Q = 'data/Questions.csv'
+    # filepath_A = 'data/Answers.csv'
+    # filepath_T = 'data/Tags.csv'
 
-    my_dict = {}
-    # my_list = []
+    # my_dict = {}
+    # # my_list = []
 
-    readQuestions(filepath_Q, my_dict)
-    readAnswers(filepath_A, my_dict)
-    readTags(filepath_T, my_dict)
+    # readQuestions(filepath_Q, my_dict)
+    # readAnswers(filepath_A, my_dict)
+    # readTags(filepath_T, my_dict)
 
 
-    # print(len(list(my_dict.keys())))
-    key_list = []
-    title_list = []
-    for key in list(my_dict.keys()):
-        title = my_dict[key].title
-        title_list.append(title)
-        key_list.append(key)
-    # print(title_list)
+    # # print(len(list(my_dict.keys())))
+    # key_list = []
+    # title_list = []
+    # for key in list(my_dict.keys()):
+    #     title = my_dict[key].title
+    #     title_list.append(title)
+    #     key_list.append(key)
+    # # print(title_list)
 
-    title_list_after_tok = tokenize(title_list)
-    # print(len(title_list_after_tok))
-    # print(title_list_after_tok[0])
-    # print(title_list_after_tok[1])
-    # print(title_list_after_tok[2])
+    # title_list_after_tok = tokenize(title_list)
+    # # print(len(title_list_after_tok))
+    # # print(title_list_after_tok[0])
+    # # print(title_list_after_tok[1])
+    # # print(title_list_after_tok[2])
 
-    query = ['Good']
-    # q = "one word".strip().split()
-    # print(q)
-    # query = tokenize(q)
+    # query = ['Good']
+    # # q = "one word".strip().split()
+    # # print(q)
+    # # query = tokenize(q)
     
-    my_model = bm25Model()
-    my_model.input_document(title_list_after_tok[0:10])
-    # print(my_model.idf)
-    score_list = my_model.get_score_list(query)
+    # my_model = bm25Model()
+    # my_model.input_document(title_list_after_tok[0:10])
+    # # print(my_model.idf)
+    # score_list = my_model.get_score_list(query)
 
-    print(score_list[0:10])
+    # print(score_list[0:10])
 
-    combined_list = list(zip(key_list, score_list))
-    sorted_combined_list = sorted(combined_list, reverse=True,  key=lambda x: x[1])
-    sorted_key_list = [x[0] for x in sorted_combined_list]
-    print(sorted_key_list[0:10])
+    # combined_list = list(zip(key_list, score_list))
+    # sorted_combined_list = sorted(combined_list, reverse=True,  key=lambda x: x[1])
+    # sorted_key_list = [x[0] for x in sorted_combined_list]
+    # print(sorted_key_list[0:10])
